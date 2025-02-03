6ebc820427b04bb2c2f71fa46d1ea5a1dd3894e5
Abdullah <abdullahismail1.work@gmail.com>
2025-02-03 13:24:47 +0000

    styling changes to reviews

--- app/Components/Testimonial.tsx
+++ app/Components/Testimonial.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 export default function Testimonial({text, name} : Props) {
 	return (
-		<div className="flex flex-col bg-slate-900 w-full text-white gap-6 py-16 mb-20 lg:py-32">
+		<div className="flex flex-col bg-slate-900 w-[90%] max-w-7xl m-auto rounded-3xl text-white gap-6 py-16 mb-20 lg:py-32">
 			<p className="mx-auto text-xl font-bold font-sans leading-7 text-center md:leading-10 md:text-3xl text-shadow-sm px-10">
       &ldquo;{text}&rdquo;
 			</p>
