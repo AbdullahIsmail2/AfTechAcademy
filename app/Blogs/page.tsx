95b43d98706982f7513a9e3541e53cfe65e59dc7
Abdullah <abdullahismail1.work@gmail.com>
2025-02-03 13:10:08 +0000

    added empty blog page

--- app/Blogs/page.tsx
+++ app/Blogs/page.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function Blogs() {
   return (
-    <div>Blogs</div>
+    <div>
+      <h1 className="text-4xl font-black font-sans text-balance tracking-tight sm:text-6xl xl:text-7xl text-center mt-20 mb-10">Blogs</h1>
+    </div>
   )
 }
